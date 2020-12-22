f4bf6a45e6313bf9488fe7396e2017256a4cec13
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-22 14:12:04 -0600

    Update windows10.ps1

--- windows10.ps1
+++ windows10.ps1
@@ -18,8 +18,8 @@ Start-Job -Name "Install and Configure Chocolatey" -ScriptBlock {
   choco upgrade all
 }
 
-Start-Sleep 10
-Start-Job -Name "Install Windows Updates" -ScriptBlock {
+Start-Sleep 15
+Start-Job -Name "Installing Windows Updates" -ScriptBlock {
   Write-Host "Install Latest Windows Updates"
   choco install pswindowsupdate
   Set-Executionpolicy -ExecutionPolicy RemoteSigned -Force
@@ -29,14 +29,12 @@ Start-Job -Name "Install Windows Updates" -ScriptBlock {
   Get-WuInstall -AcceptAll -IgnoreReboot
 }
 
-Start-Job -Name "Install Software Part 1" -Scriptblock {
+Start-Job -Name "Installing Browsers" -Scriptblock {
   Write-Host "Installing Browsers"
   choco install googlechrome firefox chromium microsoft-edge tor-Browser
+}
 
-  Write-Host "Installing Java"
-  #choco install jdk11 javaruntime
-  choco install jre8 openjdk 
-
+Start-Job -Name "Installing Administrative, Networking, and Security Tools " -Scriptblock {
   Write-Host "Installing Administration Tools"
   choco install putty winscp.install teamviewer anydesk.install sysinternals driverbooster etcher rufus.install veracrypt windirstat mysql.workbench rsat adb universal-adb-drivers windows-adk-all sql-server-management-studio laps
 
@@ -46,40 +44,18 @@ Start-Job -Name "Install Software Part 1" -Scriptblock {
   Write-Host "Installing Security Tools"
   choco install cheatengine sleuthkit hxd ida-free ghidra winlogbeat ossec-client burp-suite-free-edition zap openstego accessenum accesschk 
   
-  Write-Host "Installing Runtimes and Developer Packs"
-  choco install dotnetfx vcredist-all
-  
-  #Write-Host "Installing Baseline Tools"
-  #choco install winsecuritybaseline mbsa 
-  
-  #Write-Host "Installing AntiVirus"
-  #choco install immunet clamav 
-  
-  #Write-Host "Installing Smart Card Tools"
-  #choco install opensc
-  
-  #Write-Host "Installing YubiKey Tools"
-  #choco install yubikey-personalization-tool yubikey-manager yubikey-piv-manager
-
-  Write-host "Installing PatchMyPCHome"
-  choco install patch-my-pc --ignore-checksum
-
   Write-Host "Installing Terminals"
   #choco install docker-desktop docker-compose docker-cli azure-cli awstools.powershell awscli kubernetes-cli 
   choco install powershell4 powershell powershellhere-elevated powershell.portable microsoft-windows-terminal powertoys carbon
+}
 
-  Write-Host "Installing Chat Clients"
-  #choco install microsoft-teams.install
-  choco install rocketchat discord pidgin signal 
+Start-Job -Name "Installing Dev Tools" -Scriptblock {
+  Write-Host "Installing Java"
+  #choco install jdk11 javaruntime
+  choco install jre8 openjdk 
 
-  Write-Host "Installing Game Clients"
-  choco install steam 
-
-  Write-Host "Installing OBS"
-  choco install obs-studio obs-ndi
-
-  Write-host "Installing Media Software"
-  choco install vlc gimp k-litecodecpackfull audacity audacity-lame screentogif
+  Write-Host "Installing Runtimes and Developer Packs"
+  choco install dotnetfx vcredist-all
   
   Write-Host "Installing Hugo and Node Stack Tools"
   choco install hugo hugo-extended nodejs --force
@@ -95,15 +71,45 @@ Start-Job -Name "Install Software Part 1" -Scriptblock {
   Write-Host "Installing Windows Subsystem for Linux"
   #choco install wsl-ubuntu-2004 wsl-debiangnulinux wsl-kalilinux
   choco install wsl wsl2
+ }
+ 
+Start-Job -Name "Installing Other Tools and Software" -Scriptblock {
+  Write-host "Installing PatchMyPCHome"
+  choco install patch-my-pc --ignore-checksum
+
+  Write-Host "Installing Chat Clients"
+  #choco install microsoft-teams.install
+  choco install rocketchat discord pidgin signal 
+
+  Write-Host "Installing Game Clients"
+  choco install steam 
+
+  Write-Host "Installing OBS"
+  choco install obs-studio obs-ndi
+
+  Write-host "Installing Media Software"
+  choco install vlc gimp k-litecodecpackfull audacity audacity-lame screentogif
+
+  Write-Host "Installing Document Readers"
+  #choco install officeproplus2013
+  choco install adobereader
   
   Write-Host "Installing Misc."
   #choco install greenshot
   choco install installroot 7zip.install curl autohotkey teracopy cpu-z.install eraser
 
-  Write-Host "Installing Document Readers"
-  #choco install officeproplus2013
-  choco install adobereader
-
   Write-Host "Installing VMware"
   choco install vmwareworkstation vmware-horizon-client vmware-powercli-psmodule vmrc
+  
+  #Write-Host "Installing Baseline Tools"
+  #choco install winsecuritybaseline mbsa 
+  
+  #Write-Host "Installing AntiVirus"
+  #choco install immunet clamav 
+  
+  #Write-Host "Installing Smart Card Tools"
+  #choco install opensc
+  
+  #Write-Host "Installing YubiKey Tools"
+  #choco install yubikey-personalization-tool yubikey-manager yubikey-piv-manager
 }
