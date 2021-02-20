002513c760766f13949d59812529250f4aa3fa22
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-02-20 06:05:11 +0000

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -210,7 +210,7 @@ sudo apt-get install -y openssh-server
 BLUE "Installing vscodium..."
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
 echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list 
-sudo apt-get update && sudo apt-get install -y vscodium 
+sudo apt-get update && sudo apt-get install -y codium 
 
 BLUE "Installing anonsurf..."
 sudo apt-get install -y anonsurf 
