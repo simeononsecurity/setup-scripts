f5fe207fce0090cf37e881db40700c06b2d56e42
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-08-02 01:32:32 +0000

    Parallelize Chocolatey Installs

--- windows10.ps1
+++ windows10.ps1
@@ -58,95 +58,59 @@ refreshenv
 Start-Job -Name "Installing Software" -Scriptblock {
     Start-Sleep 60
     refreshenv
-    Write-Host "Installing Browsers"
-    choco install googlechrome firefox chromium microsoft-edge tor-Browser
 
-    Write-Host "Installing Administration Tools"
-    choco install putty winscp.install teamviewer anydesk.install sysinternals driverbooster sdio etcher rufus.install veracrypt windirstat mysql.workbench rsat sql-server-management-studio laps wumt
+    $chocopackages = @("googlechrome", "firefox", "chromium", "microsoft-edge", "tor-Browser", "putty", "winscp.install", "teamviewer", "anydesk.install", "sysinternals", "driverbooster", "sdio", "etcher", "rufus.install", "veracrypt", "windirstat", "mysql.workbench", "rsat", "sql-server-management-studio", "laps", "wumt", "openvpn", "wireguard", "wireshark", "nmap", "winbox", "tor", "cheatengine", "sleuthkit", "hxd", "ida-free", "ghidra", "ossec-client", "burp-suite-free-edition", "zap", "openstego", "accessenum", "accesschk", "sysmon", "powershell4", "powershell", "powershellhere-elevated", "powershell.portable", "microsoft-windows-terminal", "carbon", "jre8", "openjdk", "openjdk.portable", "hugo", "hugo-extended", "nodejs", "vscode", "vscodium", "vscode-ansible", "vscode-python", "chocolatey-vscode", "vscode-prettier", "vscode-java", "vscode-yaml", "vscode-haskell", "vscode-mongo", "vscode-beautify", "vscode-intellicode", "vscode-pull-request-github", "vscode-kubernetes-tools", "vscode-autofilename", "vscode-codespellchecker", "vscode-icons", "vscode-csharp", "dsc.powershellcommunity", "notepadplusplus.install", "python", "pip", "github-desktop", "gh", "git.install", "git-lfx", "gnupg", "gpg4win", "openssh", "wsl", "wsl2", "adb", "universal-adb-drivers", "windows-adk-all", "dotnetfx", "vcredist-all", "microsoft-visual-cpp-build-tools", "patch-my-pc", "rocketchat", "discord", "pidgin", "signal", "steam", "obs-studio", "obs-ndi", "vlc", "gimp", "k-litecodecpackfull", "audacity", "audacity-lame", "screentogif", "adobereader", "installroot", "7zip.install", "curl", "autohotkey", "teracopy", "cpu-z.install", "eraser", "openstego", "vmwareworkstation", "vmware-horizon-client", "vmware-powercli-psmodule", "vmrc")
 
-    Write-Host "Installing Networking Tools"
-    choco install openvpn wireguard wireshark nmap winbox tor
+    Write-Output $chocopackages | ForEach-Object -Parallel {
+        Write-Host "Installing $_"
+        Try {
+            Choco install $_ --ignore-checksums
+        } 
+        Catch {
+            Write-Host "Failed to install $_" -ForegroundColor Red -BackgroundColor Black
+        }
+    }
+    
+    <# Optional Packages
+        Write-Host "Installing Logging Tools"
+        #choco install splunk-universalforwarder winlogbeat
+    
+        Write-Host "Installing Terminals"
+        #choco install docker-desktop docker-compose docker-cli azure-cli awstools.powershell awscli kubernetes-cli 
 
-    Write-Host "Installing Security Tools"
-    #java will be installed in "C:\ProgramData\chocolatey\lib\openjdk.portable\tools\jdk-12.0.2\bin"
-    choco install cheatengine sleuthkit hxd ida-free ghidra ossec-client burp-suite-free-edition zap openstego accessenum accesschk 
-  
-    Write-Host "Installing Logging Tools"
-    #choco install splunk-universalforwarder winlogbeat
-    choco install sysmon
-  
-    Write-Host "Installing Terminals"
-    #choco install docker-desktop docker-compose docker-cli azure-cli awstools.powershell awscli kubernetes-cli 
-    choco install powershell4 powershell powershellhere-elevated powershell.portable microsoft-windows-terminal carbon
+        Write-Host "Installing Java"
+        #choco install jdk11 javaruntime
 
-    Write-Host "Installing Java"
-    #choco install jdk11 javaruntime
-    choco install jre8 openjdk openjdk.portable
-  
-    Write-Host "Installing Hugo and Node Stack Tools"
-    choco install hugo hugo-extended nodejs --force
+        Write-Host "Installing IDE and Dev Tools"
+        #choco install visualstudio2019enterprise visualstudio2017-powershelltools arduino vscode-arduino vscode-puppet vscode-ruby 
 
-    Write-Host "Installing IDE and Dev Tools"
-    #choco install visualstudio2019enterprise visualstudio2017-powershelltools arduino vscode-arduino vscode-puppet vscode-ruby 
-    choco install vscode vscodium vscode-ansible vscode-python chocolatey-vscode vscode-prettier vscode-java vscode-yaml vscode-haskell vscode-mongo vscode-beautify vscode-intellicode vscode-pull-request-github vscode-kubernetes-tools vscode-autofilename vscode-codespellchecker vscode-icons vscode-csharp dsc.powershellcommunity notepadplusplus.install python pip 
+        Write-Host "Installing GIT Tools"
+        #choco install postman markdownmonster 
 
-    Write-Host "Installing GIT Tools"
-    #choco install postman markdownmonster 
-    choco install github-desktop gh git.install git-lfx gnupg gpg4win openssh 
+        Write-Host "Installing Windows Subsystem for Linux"
+        #choco install wsl-ubuntu-2004 wsl-debiangnulinux wsl-kalilinux
 
-    Write-Host "Installing Windows Subsystem for Linux"
-    #choco install wsl-ubuntu-2004 wsl-debiangnulinux wsl-kalilinux
-    choco install wsl wsl2
-  
-    Write-Host "Installing Android Debugging Tools"
-    choco install adb universal-adb-drivers windows-adk-all
-  
-    Start-Sleep 240
-    Write-Host "Installing Runtimes and Developer Packs"
-    choco install dotnetfx vcredist-all 
-  
-    Write-Host "Installing Complile & Build Tools"
-    choco install microsoft-visual-cpp-build-tools
+        Write-Host "Installing Chat Clients"
+        #choco install microsoft-teams.install
 
-    Write-host "Installing PatchMyPCHome"
-    choco install patch-my-pc --ignore-checksum
-
-    Write-Host "Installing Chat Clients"
-    #choco install microsoft-teams.install
-    choco install rocketchat discord pidgin signal 
-
-    Write-Host "Installing Game Clients"
-    choco install steam 
-
-    Write-Host "Installing OBS"
-    choco install obs-studio obs-ndi
-
-    Write-host "Installing Media Software"
-    choco install vlc gimp k-litecodecpackfull audacity audacity-lame screentogif
-
-    Write-Host "Installing Document Readers"
-    #choco install officeproplus2013
-    choco install adobereader
-  
-    Write-Host "Installing Misc."
-    #choco install greenshot
-    choco install installroot 7zip.install curl autohotkey teracopy cpu-z.install eraser openstego
-  
-    Start-Sleep 240
-    Write-Host "Installing VMware"
-    choco install vmwareworkstation vmware-horizon-client vmware-powercli-psmodule vmrc
-  
-    #Write-Host "Installing Baseline Tools"
-    #choco install winsecuritybaseline mbsa 
-  
-    #Write-Host "Installing AntiVirus"
-    #choco install immunet clamav 
-  
-    #Write-Host "Installing Smart Card Tools"
-    #choco install opensc
-  
-    #Write-Host "Installing YubiKey Tools"
-    #choco install yubikey-personalization-tool yubikey-manager yubikey-piv-manager
+        Write-Host "Installing Document Readers"
+        #choco install officeproplus2013
+    
+        Write-Host "Installing Misc."
+        #choco install greenshot
+    
+        #Write-Host "Installing Baseline Tools"
+        #choco install winsecuritybaseline mbsa 
+    
+        #Write-Host "Installing AntiVirus"
+        #choco install immunet clamav 
+    
+        #Write-Host "Installing Smart Card Tools"
+        #choco install opensc
+    
+        #Write-Host "Installing YubiKey Tools"
+        #choco install yubikey-personalization-tool yubikey-manager yubikey-piv-manager
+    #>
 }
 
 Start-Job -Name "Configuring Windows - Optimizations, Debloating, and Hardening" -ScriptBlock {
